f02aaa8e18809f15f3c04038386eb428867da730
iystay <yasinusprime@gmail.com>
2025-12-14 13:33:41 +0300

    utils.js

--- utils.js
+++ utils.js
@@ -1,1 +1,48 @@
+// Yardımcı Fonksiyonlar - Üst Seviye
+const Utils = {
+    randInt(min, max){
+        return Math.floor(Math.random()*(max-min+1))+min;
+    },
 
+    randChoice(arr){
+        return arr[Utils.randInt(0, arr.length-1)];
+    },
+
+    normalize(text){
+        return text.toLowerCase().trim();
+    },
+
+    wait(ms){
+        return new Promise(resolve => setTimeout(resolve, ms));
+    },
+
+    safeCalc(expression){
+        try{
+            // Sadece güvenli karakterler: rakam, operatör, parantez, boşluk
+            if(!/^[0-9+\-*/().\s]+$/.test(expression)) return "Geçersiz ifade";
+            return Function('"use strict"; return ('+expression+')')();
+        }catch(e){
+            return "Hata: "+e.message;
+        }
+    },
+
+    clamp(val, min, max){
+        return Math.min(Math.max(val, min), max);
+    },
+
+    randomColor(){
+        const r=Utils.randInt(0,255);
+        const g=Utils.randInt(0,255);
+        const b=Utils.randInt(0,255);
+        return rgb(${r},${g},${b});
+    },
+
+    delayMap(arr, callback, interval=300){
+        return arr.reduce((promise, item)=>{
+            return promise.then(()=>callback(item)).then(()=>Utils.wait(interval));
+        }, Promise.resolve());
+    }
+};
+
+// Örnek export
+const utils = Utils;
